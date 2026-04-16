6af3c11cda932ad7bde64c36fb3c8af194cf0da8
Kurnia Muhamad <omenkzz@gmail.com>
2026-04-16 23:18:21 +0700

    feat: implement search and filtering in properties catalog

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -2,8 +2,28 @@ class PropertiesController < ApplicationController
   allow_unauthenticated_access only: [:index, :show]
 
   def index
-    @properties = Property.active.all
+    @properties = Property.where(status: :active)
+    @regions = Region.all
+    @property_types = Property.property_types.keys
+
+    # Search by query
+    if params[:query].present?
+      @properties = @properties.where("title ILIKE ? OR description ILIKE ?", "%#{params[:query]}%", "%#{params[:query]}%")
+    end
+
+    # Filter by type
+    @properties = @properties.where(property_type: params[:type]) if params[:type].present?
+    
+    # Filter by listing type
+    @properties = @properties.where(listing_type: params[:listing_type]) if params[:listing_type].present?
+
+    # Filter by region
+    @properties = @properties.where(region_id: params[:region_id]) if params[:region_id].present?
+
+    # Sort
+    @properties = @properties.order(created_at: :desc)
   end
+
 
   def show
     @property = Property.find(params[:id])
